92e746d9c8304a3e45df2f9e3b42c6429afc3f63
Jiaqi Zhu <zhu442@purdue.edu>
2016-10-09 16:39:26 -0400

    Raycast system finish.In game data interface done.

---
+++ Assets/Controller/GameController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    // Use this for initialization
+    public Hopperscript hopper0;
+    public Hopperscript hopper1;
+    public Hopperscript hopper2;
+    public Hopperscript hopper3;
+    public  int currentscore = 0;//score for now
+    public  int currentcombo = 0;//combo for now
+    public int goodincre = 10;//socre to add for good
+    public int perfectincre = 20;//score to add for perfect
+    
+
+    void Start () {
+	
+	}
+    public void addScore(int incre)//add score from each hopper to the controller
+    {
+        currentscore += incre;
+    }
+    public void addCombo()
+    {
+        currentcombo++;
+    }
+    public void clearcombo()//called when miss happened
+    {
+        currentcombo = 0;
+    }
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(currentcombo > 0 && currentcombo %50 == 0)
+        {
+            goodincre *= 2;
+            perfectincre *= 2;
+            
+        }
+        if(currentcombo == 0)
+        {
+            goodincre = 10;
+            perfectincre = 20;
+        }
+	
+	}
+}

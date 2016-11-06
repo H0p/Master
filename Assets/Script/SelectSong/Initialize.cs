0900d6a3ba7ca4f987c8afe7354d63e183abc660
kathy789 <ji79@purdue.edu>
2016-11-06 14:12:12 -0500

    open dirctory to get the n number of musci files

--- Assets/Script/SelectSong/Initialize.cs
+++ Assets/Script/SelectSong/Initialize.cs
@@ -3,10 +3,16 @@ using System.Collections;
 using System.IO;
 
 public class Initialize : MonoBehaviour {
-
+	
+	public GameObject prefab;
     // Use this for initialization
     private string filetrack;
 	void Start () {
+
+		// get the number of musci files 
+		int num = getNumber();
+
+		/*
         filetrack = System.Environment.CurrentDirectory;
         Debug.Log(filetrack);
         DirectoryInfo current = new DirectoryInfo(filetrack);
@@ -16,8 +22,22 @@ public class Initialize : MonoBehaviour {
         {
             Debug.Log(descent[i].Name);
         }
+        */
 
     }
+
+	int getNumber() {
+
+		string[] musicfiles = Directory.GetFiles(System.Environment.CurrentDirectory + "/Assets/testmusic", "*.mp3");
+		int count = musicfiles.Length;
+		Debug.Log("count: " + count.ToString ());
+		for (int i = 0; i < count; i++) {
+
+			Debug.Log (musicfiles [i]);
+		}
+
+		return count;
+	}
 	
 	// Update is called once per frame
 	void Update () {

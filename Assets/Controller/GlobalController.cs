148f9caadb130a67894713c421fa9548e9de23eb
Jiaqi Zhu <zhu442@purdue.edu>
2016-11-09 22:55:34 -0500

    generator by Junran Xu

--- Assets/Controller/GlobalController.cs
+++ Assets/Controller/GlobalController.cs
@@ -1,15 +1,40 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Threading;
+using parser;
 
-public class GlobalController : MonoBehaviour {
+namespace global
+{
+    public class GlobalController : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+            //parse at the beginning of the welcome scene
+
+            Debug.Log("controller starts");
+
+            Thread thread = new Thread(() => OSUb_Parser.ParseOsuFile(@".\Osu\sample.osu"));
+            thread.Start();
+            
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //When a song was select in selectSong scene, the bmID was changed to the BeatMapId of that song, 
+            //and the trackNum should be set to the number of tracks according to difficulty
+
+        }
+    }
 }
